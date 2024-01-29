eceb22ecb4e8fa97c5741d2346f71d4d1a52835a
Agoyal011 <eng.aniketgoyal@gmail.com>
2024-01-29 02:16:17 -0500

    Navigation component completed

---
+++ src/Componen/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='nav-bar'>
+    
+    <img src="./Nav bar.png" className="App-logo" alt="logo" />
+
+    </div>
+
+    
+  )
+}
+
+export default Navigation
